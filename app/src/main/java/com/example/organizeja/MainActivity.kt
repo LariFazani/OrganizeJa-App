d127ff199fffd28b507209f26126e8cbdf7d3365
Larissa <larifazani@gmail.com>
2025-09-10 22:49:15 -0300

    Ajuste nas telas Transações e Adicionar Transações.

--- app/src/main/java/com/example/organizeja/MainActivity.kt
+++ app/src/main/java/com/example/organizeja/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.organizeja
 
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.organizeja.databinding.ActivityMainBinding
@@ -16,18 +18,32 @@ class MainActivity : AppCompatActivity() {
     // Método onCreate
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         // Inflar o Layout
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         // Referência à BottomNavigationView
         val navView: BottomNavigationView = binding.navView
-
         // Configuração do NavController
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
 
         // Vincular a BottomNavigationView ao NavController
         navView.setupWithNavController(navController)
+
+        // Adiciona um listener para o destino da navegação
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                // Se o destino for a tela de adicionar transação, oculte a ActionBar
+                R.id.navigation_adicionar_transacao -> {
+                    supportActionBar?.hide()
+                    binding.navView.visibility = View.GONE
+                }
+                // Para todas as outras telas (início, transações, configurações), mostre a ActionBar
+                else -> {
+                    supportActionBar?.show()
+                    binding.navView.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/organizeja/ui/transacoes/AdicionarTransacaoFragment.kt
+++ app/src/main/java/com/example/organizeja/ui/transacoes/AdicionarTransacaoFragment.kt
@@ -1,9 +1,12 @@
 package com.example.organizeja.ui.transacoes
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.organizeja.R
@@ -12,7 +15,8 @@ import com.google.android.material.datepicker.MaterialDatePicker
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
-import android.widget.ArrayAdapter
+import java.text.NumberFormat
+
 
 class AdicionarTransacaoFragment : Fragment() {
 
@@ -30,11 +34,17 @@ class AdicionarTransacaoFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Configura o Spinner com as categorias
         setupCategoriaSpinner()
 
-        // Configura o DatePicker
-        setupDatePicker()
+        // Remove o clique do EditText e adiciona o clique ao ícone de calendário
+        binding.dataInputLayout.setEndIconOnClickListener {
+            showDatePicker()
+        }
+
+        // Adiciona o TextWatcher para a formatação da data
+        setupDateTextWatcher()
+
+        setupValorEditText()
 
         // Lida com o clique do botão de salvar
         binding.buttonSalvar.setOnClickListener {
@@ -46,9 +56,10 @@ class AdicionarTransacaoFragment : Fragment() {
         // Lida com o clique do botão de cancelar
         binding.buttonCancelar.setOnClickListener {
             // Lógica para cancelar e fechar a tela
-            // Exemplo: navigation.findNavController().popBackStack()
+            //navigation.findNavController().popBackStack()
             Toast.makeText(context, "Cancelar Clicado!", Toast.LENGTH_SHORT).show()
         }
+
     }
 
     private fun setupCategoriaSpinner() {
@@ -59,21 +70,108 @@ class AdicionarTransacaoFragment : Fragment() {
         binding.spinnerCategoria.adapter = adapter
     }
 
-    private fun setupDatePicker() {
-        binding.dataEditText.setOnClickListener {
-            val datePicker = MaterialDatePicker.Builder.datePicker()
-                .setTitleText("Selecione a data da transação")
-                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
-                .build()
+    private fun setupDateTextWatcher() {
+        binding.dataEditText.addTextChangedListener(object : TextWatcher {
+            private var isFormatting = false
+            private var isDeleting = false
 
-            datePicker.addOnPositiveButtonClickListener { selection ->
-                val date = Date(selection)
-                val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale("pt", "BR"))
-                binding.dataEditText.setText(dateFormat.format(date))
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                isDeleting = count > after
             }
 
-            datePicker.show(requireFragmentManager(), "DatePicker")
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                if (isFormatting || isDeleting) {
+                    return
+                }
+
+                isFormatting = true
+
+                val text = s.toString().replace("/", "")
+
+                if (text.length > 8) {
+                    s?.delete(8, text.length)
+                    isFormatting = false
+                    return
+                }
+
+                val formatted = StringBuilder()
+                formatted.append(text)
+
+                if (formatted.length >= 2) {
+                    formatted.insert(2, "/")
+                }
+                if (formatted.length >= 5) {
+                    formatted.insert(5, "/")
+                }
+
+                val finalFormatted = formatted.toString()
+                if (finalFormatted != s.toString()) {
+                    val selection = binding.dataEditText.selectionStart
+                    binding.dataEditText.setText(finalFormatted)
+                    binding.dataEditText.setSelection(selection + (finalFormatted.length - s.toString().length))
+                }
+
+                isFormatting = false
+            }
+        })
+    }
+
+    // Altere a função setupDatePicker para showDatePicker e remova a linha de setOnClickListener
+    private fun showDatePicker() {
+        val datePicker = MaterialDatePicker.Builder.datePicker()
+            .setTitleText("Selecione a data da transação")
+            .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+            .build()
+
+        datePicker.addOnPositiveButtonClickListener { selection ->
+            val date = Date(selection)
+            val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale("pt", "BR"))
+            binding.dataEditText.setText(dateFormat.format(date))
         }
+
+        datePicker.show(requireFragmentManager(), "DatePicker")
+    }
+
+    private fun setupValorEditText() {
+        val locale = Locale("pt", "BR") // Usa a localização para o Brasil (Real)
+        val currencyFormatter = NumberFormat.getCurrencyInstance(locale)
+
+        // Adicione um TextWatcher ao EditText de valor
+        binding.valorEditText.addTextChangedListener(object : TextWatcher {
+            private var current = ""
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Não é necessário implementar aqui
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // Não é necessário implementar aqui
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (s.toString() != current) {
+                    binding.valorEditText.removeTextChangedListener(this)
+
+                    val cleanString = s.toString().replace("[R$,.\\s]".toRegex(), "")
+
+                    if (cleanString.isNotEmpty()) {
+                        val parsed = cleanString.toDouble() / 100
+                        val formatted = currencyFormatter.format(parsed)
+
+                        current = formatted
+                        binding.valorEditText.setText(formatted)
+                        binding.valorEditText.setSelection(formatted.length)
+                    } else {
+                        current = ""
+                        binding.valorEditText.setText("")
+                    }
+
+                    binding.valorEditText.addTextChangedListener(this)
+                }
+            }
+        })
     }
 
     override fun onDestroyView() {
